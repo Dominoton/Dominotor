6792d04e311679ed782f85e6be5981a7647b01eb
Dominoton <github@dominoton.clothlen.com>
2017-04-04 10:39:54 -0500

    remove comments; remove messing with include path

--- src/index.php
+++ src/index.php
@@ -13,7 +13,7 @@
 
 set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] );
 
-$dpType = "index";
+define('DPTYPE',"index")
 // We are going to dirname( __FILE__ ) here because __DIR__ is only in PHP5.3+.
 // Yes. dirname(__FILE__) is a verb.
 require dirname(__FILE__).'/domicode/Include'; // We need to find the master include file.
@@ -24,23 +24,13 @@ require dirname(__FILE__).'/domicode/beginWeb'; // We need to begin the webpage.
 include __DIR__.'/domicode/includes/Header';
 include __DIR__.'/domicode/includes/Sidebar';
 
-// TODO: Change the IF to something different, maybe?
-/*if(isset($dpType) && $dpType === "index"){
-	$queryGetThreads = $conn->query("SELECT thread_id, thread_id2, thread_subject, thread_date, thread_realm, thread_by, thread_flair, thread_flair_id FROM dThreads");
-	$rowGetThreads=$queryGetThreads->fetch_array();
-	$countGetThreads = $queryGetThreads->num_rows; // if email/password are correct returns must be 1 row
-	if($countGetThreads===1){
-		echo 'yes';
-	}
-}*/
 
 
 $queryGetThreads = $conn->query('SELECT * FROM dThreads');
 $rowGetThreads = $queryGetThreads->fetch_array();
 $countGetThreads = $queryGetThreads->num_rows;
-if($countGetThreads>1){
+if($countGetThreads>0){
 	while($rowGetThreads=$queryGetThreads->fetch_array(MYSQLI_BOTH)){
-		//$tcRow['comment_by'] = $cmtCA['u_name'];
 		$buildThreads = $rowGetThreads;
 		if(isset($dUserWorking)){
 			$buildThreads['u_lvl'] = $u_lvl;
